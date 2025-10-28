742a57aa8da2ddc5c2e7444871f08a3f82436494
ankit-0903 <1by22cs022@bmsit.in>
2025-10-29 03:41:51 +0530

    UI: Designed Party Detail Screen

---
+++ lib/features/parties/vm/party.vm.g.dart
@@ -0,0 +1,131 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'party.vm.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
+@ProviderFor(PartyViewModel)
+const partyViewModelProvider = PartyViewModelProvider._();
+
+final class PartyViewModelProvider
+    extends $AsyncNotifierProvider<PartyViewModel, List<PartyDetails>> {
+  const PartyViewModelProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'partyViewModelProvider',
+        isAutoDispose: true,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$partyViewModelHash();
+
+  @$internal
+  @override
+  PartyViewModel create() => PartyViewModel();
+}
+
+String _$partyViewModelHash() => r'cb805b8852e8d244293a12fb7f308f495f01f676';
+
+abstract class _$PartyViewModel extends $AsyncNotifier<List<PartyDetails>> {
+  FutureOr<List<PartyDetails>> build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref =
+        this.ref as $Ref<AsyncValue<List<PartyDetails>>, List<PartyDetails>>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<AsyncValue<List<PartyDetails>>, List<PartyDetails>>,
+              AsyncValue<List<PartyDetails>>,
+              Object?,
+              Object?
+            >;
+    element.handleValue(ref, created);
+  }
+}
+
+@ProviderFor(partyById)
+const partyByIdProvider = PartyByIdFamily._();
+
+final class PartyByIdProvider
+    extends
+        $FunctionalProvider<
+          AsyncValue<PartyDetails?>,
+          PartyDetails?,
+          FutureOr<PartyDetails?>
+        >
+    with $FutureModifier<PartyDetails?>, $FutureProvider<PartyDetails?> {
+  const PartyByIdProvider._({
+    required PartyByIdFamily super.from,
+    required String super.argument,
+  }) : super(
+         retry: null,
+         name: r'partyByIdProvider',
+         isAutoDispose: true,
+         dependencies: null,
+         $allTransitiveDependencies: null,
+       );
+
+  @override
+  String debugGetCreateSourceHash() => _$partyByIdHash();
+
+  @override
+  String toString() {
+    return r'partyByIdProvider'
+        ''
+        '($argument)';
+  }
+
+  @$internal
+  @override
+  $FutureProviderElement<PartyDetails?> $createElement(
+    $ProviderPointer pointer,
+  ) => $FutureProviderElement(pointer);
+
+  @override
+  FutureOr<PartyDetails?> create(Ref ref) {
+    final argument = this.argument as String;
+    return partyById(ref, argument);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is PartyByIdProvider && other.argument == argument;
+  }
+
+  @override
+  int get hashCode {
+    return argument.hashCode;
+  }
+}
+
+String _$partyByIdHash() => r'd33db2377863a7dc2199187990bcaf95c61913e6';
+
+final class PartyByIdFamily extends $Family
+    with $FunctionalFamilyOverride<FutureOr<PartyDetails?>, String> {
+  const PartyByIdFamily._()
+    : super(
+        retry: null,
+        name: r'partyByIdProvider',
+        dependencies: null,
+        $allTransitiveDependencies: null,
+        isAutoDispose: true,
+      );
+
+  PartyByIdProvider call(String partyId) =>
+      PartyByIdProvider._(argument: partyId, from: this);
+
+  @override
+  String toString() => r'partyByIdProvider';
+}
